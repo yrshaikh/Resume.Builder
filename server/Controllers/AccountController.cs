4bc32057c479176ac87e440835c63195adb775f1
Yasser <yasser.shaikh@agoda.com>
2019-06-30 20:49:09 +0700

    Done with .idea shit.

--- server/Controllers/AccountController.cs
+++ server/Controllers/AccountController.cs
@@ -81,48 +81,47 @@ namespace WebApi.Controllers
             }
         }
         
-//        [HttpGet]
-//        public IActionResult GetAll()
-//        {
-//            var users =  _userService.GetAll();
-//            var userDtos = _mapper.Map<IList<UserDto>>(users);
-//            return Ok(userDtos);
-//        }
-//
-//        [HttpGet("{id}")]
-//        public IActionResult GetById(int id)
-//        {
-//            var user =  _userService.GetById(id);
-//            var userDto = _mapper.Map<UserDto>(user);
-//            return Ok(userDto);
-//        }
-//
-//        [HttpPut("{id}")]
-//        public IActionResult Update(int id, [FromBody]UserDto userDto)
-//        {
-//            // map dto to entity and set id
-//            var user = _mapper.Map<User>(userDto);
-//            user.Id = id;
-//
-//            try 
-//            {
-//                // save 
-//                _userService.Update(user, userDto.Password);
-//                return Ok();
-//            } 
-//            catch(AppException ex)
-//            {
-//                // return error message if there was an exception
-//                return BadRequest(ex.Message);
-//            }
-//        }
-//
-//        [HttpDelete("{id}")]
-//        public IActionResult Delete(int id)
-//        {
-//            _userService.Delete(id);
-//            return Ok();
-//        }
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users =  _userService.GetAll();
+            var userDtos = _mapper.Map<IList<UserDto>>(users);
+            return Ok(userDtos);
+        }
 
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var user =  _userService.GetById(id);
+            var userDto = _mapper.Map<UserDto>(user);
+            return Ok(userDto);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody]UserDto userDto)
+        {
+            // map dto to entity and set id
+            var user = _mapper.Map<User>(userDto);
+            user.Id = id;
+
+            try 
+            {
+                // save 
+                _userService.Update(user, userDto.Password);
+                return Ok();
+            } 
+            catch(AppException ex)
+            {
+                // return error message if there was an exception
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userService.Delete(id);
+            return Ok();
+        }
     }
 }
