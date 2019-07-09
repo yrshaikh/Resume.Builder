e1b02ef6fe03c378482ea75c87aa774066f8e790
Yasser <yasser.shaikh@agoda.com>
2019-07-09 22:28:27 +0700

    Added home page.

--- resumebuilder.client/src/App.tsx
+++ resumebuilder.client/src/App.tsx
@@ -20,15 +20,9 @@ const App: React.FC = () => {
             <Menu.Item key="3">nav 3</Menu.Item>
           </Menu>
         </Header>
-        <Content style={{ padding: '0 50px' }}>
-          <Breadcrumb style={{ margin: '16px 0' }}>
-            <Breadcrumb.Item>Home</Breadcrumb.Item>
-            <Breadcrumb.Item>List</Breadcrumb.Item>
-            <Breadcrumb.Item>App</Breadcrumb.Item>
-          </Breadcrumb>
-          <div style={{ background: '#fff', padding: 24, minHeight: 280 }}>Content</div>
+        <Content className="layout__content">
+          <div className="layout__contentDiv" >Content</div>
         </Content>
-        <Footer style={{ textAlign: 'center' }}>Ant Design ©2018 Created by Ant UED</Footer>
       </Layout>
   );
 }
