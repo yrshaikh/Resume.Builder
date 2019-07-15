2d1f82893cc8096c6456ac8d0312499b2f3e1bb3
Yasser <yasser.shaikh@agoda.com>
2019-07-15 09:18:27 +0700

    Breaking down layout into more components.

---
+++ resumebuilder.client/src/Pages/Layouts/components/AppMenu/AppMenu.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import './AppMenu.scss';
+import { Menu } from 'antd';
+
+const AppMenu: React.FC = () => {
+    const selectedMenu = ['1'];
+    return (
+        <Menu className="AppMenu"
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={selectedMenu}
+        >
+            <Menu.Item key="1">Home</Menu.Item>
+            <Menu.Item key="2">Features</Menu.Item>
+            <Menu.Item className="AppMenu__Item--right AppMenu__Item--callToAction" key="3">Create an
+                    account</Menu.Item>
+            <Menu.Item className="AppMenu__Item--right" key="4">Sign In</Menu.Item>
+        </Menu>
+    );
+};
+
+export default AppMenu;
